7213c5ee27f4aa97e7c7faa2c2c26c9e7c4ea56a
Ali Jaffar <alij123402@gmail.com>
2023-01-09 09:29:02 -0500

    Initial Commit

---
+++ Mizu/MizuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MizuApp.swift
+//  Mizu
+//
+//  Created by Ali Jaffar on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct MizuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
