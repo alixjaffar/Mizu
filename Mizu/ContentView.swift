bf58f322a8f740c8127e1a835f58372677bd16ab
Ali Jaffar <alij123402@gmail.com>
2023-01-10 01:41:07 -0500

    Initial Code

--- Mizu/ContentView.swift
+++ Mizu/ContentView.swift
@@ -8,14 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var quotes = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text(quotes)
+                .navigationTitle(quotes)
+                .onAppear(perform: randQuotes)
+                .font(.largeTitle.weight(.bold))
+                .foregroundColor(Color.white)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity) // 1
+        .accentColor(Color.black)
+        .background(Color.red)
+}
+    
+    
+    
+    func randQuotes(){
+        if let startsWordsURL = Bundle.main.url(forResource: "compliments", withExtension: "txt"){
+            if let startWords = try? String(contentsOf: startsWordsURL){
+                let allWords = startWords.components(separatedBy: "\n")
+                quotes = allWords.randomElement() ?? "You're the Best!"
+                return
+            }
+        }
+        fatalError("u messed up with the txt file smh")
     }
 }
 
